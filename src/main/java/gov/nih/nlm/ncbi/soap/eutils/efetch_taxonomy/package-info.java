0f2de2fc316602f7c50eb1b01f117f200a290e23
Tim Angus <tim.angus@roslin.ed.ac.uk>
2014-10-30 10:30:33 +0000

    Remove some more stuff that we probably shouldn't be claiming ownership of

--- src/main/java/gov/nih/nlm/ncbi/soap/eutils/efetch_taxonomy/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ncbi.nlm.nih.gov/soap/eutils/efetch_taxonomy", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package gov.nih.nlm.ncbi.soap.eutils.efetch_taxonomy;
